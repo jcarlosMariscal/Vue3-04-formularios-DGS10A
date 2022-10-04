9fd41ce2fff71cf08b0f465538a5101f6ba31ca4
jcarlosmariscal <juanc.ramirezm16@gmail.com>
2022-10-04 13:14:10 -0500

    04-formularios | Clase 30 de septiembre 2022

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -12,6 +12,7 @@ export default {
         proyecto: this.proyecto,
         tipo: this.tipo,
         urgente: this.urgente,
+        completado: false,
       };
       this.proyectos.push(proyecto);
 
@@ -19,65 +20,90 @@ export default {
       this.tipo = "";
       this.urgente = false;
     },
+    cambiarEstado(proyecto, campo) {
+      // this.proyectos[id].urgente = !this.proyectos[id].urgente;
+      proyecto[campo] = !proyecto[campo];
+    },
+  },
+  computed: {
+    numeroProyectos() {
+      return this.proyectos.length;
+    },
   },
 };
 </script>
 
 <template>
-  <form @submit.prevent="registrarProyecto">
-    <div class="mb-3">
-      <label class="form-label">Proyecto</label>
-      <input
-        v-model.trim="proyecto"
-        type="text"
-        class="form-control"
-        required
-      />
+  <div class="row">
+    <div class="col-12 col-md-4">
+      <form @submit.prevent="registrarProyecto">
+        <div class="mb-3">
+          <label class="form-label">Proyecto</label>
+          <input
+            v-model.trim="proyecto"
+            type="text"
+            class="form-control"
+            required
+          />
+        </div>
+
+        <div class="mb-3">
+          <label class="form-label">Actividad</label>
+          <select v-model.trim="tipo" class="form-select" required>
+            <option disabled selected value="">
+              Selecciona un tipo de actividad
+            </option>
+            <option>Aplicaciones Web con Vue.js</option>
+            <option>Backend Services con Node</option>
+            <option>App móvil con React Native</option>
+          </select>
+        </div>
+
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-check-label"
+            >Urgente</label
+          >
+          <input v-model="urgente" type="checkbox" class="form-checkbox" />
+        </div>
+
+        <button type="submit" class="btn btn-primary">Guardar</button>
+      </form>
     </div>
-
-    <div class="mb-3">
-      <label class="form-label">Actividad</label>
-      <select v-model.trim="tipo" class="form-select" required>
-        <option disabled selected value="">
-          Selecciona un tipo de actividad
-        </option>
-        <option>Aplicaciones Web con Vue.js</option>
-        <option>Backend Services con Node</option>
-        <option>App móvil con React Native</option>
-      </select>
+    <div class="col-12 col-md-8">
+      <h3>Total de proyectos: {{ numeroProyectos }}</h3>
+      <div class="table-responsive">
+        <table class="table table-dark table-hover">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Proyecto</th>
+              <th>Tipo</th>
+              <th>Urgente</th>
+              <th>Completado</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(proyecto, index) in proyectos" :key="index">
+              <td>{{ index + 1 }}</td>
+              <td>{{ proyecto.proyecto }}</td>
+              <td>{{ proyecto.tipo }}</td>
+              <td
+                @click="cambiarEstado(proyecto, 'urgente')"
+                :class="proyecto.urgente ? 'bg-success' : 'bg-danger'"
+              >
+                {{ proyecto.urgente ? "Sí" : "No" }}
+              </td>
+              <td
+                @click="cambiarEstado(proyecto, 'completado')"
+                :class="proyecto.completado ? 'bg-success' : 'bg-danger'"
+              >
+                {{ proyecto.completado ? "Completado" : "Incompleto" }}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
-
-    <div class="mb-3">
-      <label for="exampleInputPassword1" class="form-check-label"
-        >Urgente</label
-      >
-      <input v-model="urgente" type="checkbox" class="form-checkbox" />
-    </div>
-
-    <button type="submit" class="btn btn-primary">Guardar</button>
-  </form>
-  <hr />
-
-  <div class="table-responsive">
-    <table class="table table-dark">
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Proyecto</th>
-          <th>Tipo</th>
-          <th>Urgente</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Laravel</td>
-          <td>Larabel</td>
-          <!-- <td class="bg-success">Si / No</td> -->
-          <td class="bg-danger">Si / No</td>
-        </tr>
-      </tbody>
-    </table>
   </div>
 </template>
 <!-- 
