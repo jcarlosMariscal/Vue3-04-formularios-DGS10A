329f157d3207d640c8638dd215b2c5d17a3c5363
jcarlosmariscal <juanc.ramirezm16@gmail.com>
2022-12-04 19:32:04 -0600

    nuevo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="row">
     <div class="col-12 col-md-3">
-      <h1>Formularios</h1>
+      <h1>Mis proyectos</h1>
     </div>
     <div class="col-12 col-md-6">
       <img width="100" :src="url" :alt="msg" />
